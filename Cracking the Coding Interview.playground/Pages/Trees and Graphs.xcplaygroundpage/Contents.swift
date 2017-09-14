54cca42b882669654e6c065dd27813c526ee2059
Matt Lewin <matt@lewin.us>
2017-09-14 16:15:22 -0700

    4.1 Updated to include depth-first search, in addition to breadth-first search

--- Cracking the Coding Interview.playground/Pages/Trees and Graphs.xcplaygroundpage/Contents.swift
+++ Cracking the Coding Interview.playground/Pages/Trees and Graphs.xcplaygroundpage/Contents.swift
@@ -5,15 +5,18 @@ import Foundation
 /*: ---
  ## 4.1 Route Between Nodes: Given a directed graph, design an algorithm to find out whether there is a route between two nodes.
  
- * Callout(Thoughts): This sounds like a simple breadth-first-search from the `source` to the `target`. That is, traverse each adjacent node from `source` looking for `target`. If `target` isn't found, check each adjacent node of the nodes adjacent to `target`. Repeat until `target` is found or all adjacent nodes checked.
+ * Callout(Thoughts): This sounds like a simple breadth-first search from the `source` to the `target`. That is, traverse each adjacent node from `source` looking for `target`. If `target` isn't found, check each adjacent node of the nodes adjacent to `target`. Repeat until `target` is found or all adjacent nodes checked.
+ 
+ 
+ * Callout(Further Thoughts): Because we are not seeking the shortest path, simply whether there *is* a path, there is no reason we must use a breadth-first search instead of a depth-first search. Thus, I will implement both.
  */
-func pathExists<T: Comparable>(from: GraphNode<T>, to: GraphNode<T>) -> Bool {
+func bfsPathExists<T: Comparable>(from: GraphNode<T>, to: GraphNode<T>) -> Bool {
     var toVisit = [GraphNode<T>]()
     toVisit.append(from)
 
     repeat {
         let aNode = toVisit.remove(at: 0)
-        guard aNode.value != to.value else { return true }
+        guard aNode !== to else { return true }
         guard !aNode.visited else { continue }
         aNode.visited = true
 
@@ -26,12 +29,26 @@ func pathExists<T: Comparable>(from: GraphNode<T>, to: GraphNode<T>) -> Bool {
     return false
 }
 
+func dfsPathExists<T: Comparable>(from: GraphNode<T>, to: GraphNode<T>) -> Bool {
+    guard from !== to else { return true }
+    from.visited = true
+
+    for node in from.adjacent {
+        guard !node.visited else { continue }
+        guard dfsPathExists(from: node, to: to) else {
+            continue
+        }
+        return true
+    }
+    return false
+}
+
 /*:
- To test `pathExists()`, we will use this simple graph
+ To test `bfsPathExists(from:to:)` and `dfsPathExists(from:to:)`, we will use this simple graph
 
  ![Simple Graph](Simple%20Graph.png)
  */
-print("\n\n---------- 4.1 Route Between Nodes ----------")
+print("---------- 4.1 Route Between Nodes ----------")
 func createGraph() -> [GraphNode<Int>] {
     let nodes = [
         GraphNode<Int>(value: 0),
@@ -53,16 +70,24 @@ func createGraph() -> [GraphNode<Int>] {
 }
 
 var graph = createGraph()
-print("pathExists(from: graph[0], to: graph[3]): \(pathExists(from: graph[0], to: graph[3]))")
+print("bfsPathExists(from: graph[0], to: graph[3]): \(bfsPathExists(from: graph[0], to: graph[3]))")
+graph = createGraph()
+print("dfsPathExists(from: graph[0], to: graph[3]): \(dfsPathExists(from: graph[0], to: graph[3]))")
 
 graph = createGraph()
-print("pathExists(from: graph[0], to: graph[2]): \(pathExists(from: graph[0], to: graph[2]))")
+print("bfsPathExists(from: graph[0], to: graph[2]): \(bfsPathExists(from: graph[0], to: graph[2]))")
+graph = createGraph()
+print("dfsPathExists(from: graph[0], to: graph[2]): \(dfsPathExists(from: graph[0], to: graph[2]))")
 
 graph = createGraph()
-print("pathExists(from: graph[1], to: graph[5]): \(pathExists(from: graph[1], to: graph[5]))")
+print("bfsPathExists(from: graph[1], to: graph[5]): \(bfsPathExists(from: graph[1], to: graph[5]))")
+graph = createGraph()
+print("dfsPathExists(from: graph[1], to: graph[5]): \(dfsPathExists(from: graph[1], to: graph[5]))")
 
 graph = createGraph()
-print("pathExists(from: graph[2], to: graph[4]): \(pathExists(from: graph[2], to: graph[4]))")
+print("bfsPathExists(from: graph[2], to: graph[4]): \(bfsPathExists(from: graph[2], to: graph[4]))")
+graph = createGraph()
+print("dfsPathExists(from: graph[2], to: graph[4]): \(dfsPathExists(from: graph[2], to: graph[4]))")
 
 //: ---
 //: [Previous](@previous)
