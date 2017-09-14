ff984faa3e39c83569083f93d9c0294135d4385f
Matt Lewin <matt@lewin.us>
2017-09-14 13:28:23 -0700

    4.1 Route Between Nodes

--- Cracking the Coding Interview.playground/Pages/Trees and Graphs.xcplaygroundpage/Contents.swift
+++ Cracking the Coding Interview.playground/Pages/Trees and Graphs.xcplaygroundpage/Contents.swift
@@ -3,8 +3,66 @@
 import Foundation
 
 /*: ---
- ## 4.1
+ ## 4.1 Route Between Nodes: Given a directed graph, design an algorithm to find out whether there is a route between two nodes.
+ 
+ * Callout(Thoughts): This sounds like a simple breadth-first-search from the `source` to the `target`. That is, traverse each adjacent node from `source` looking for `target`. If `target` isn't found, check each adjacent node of the nodes adjacent to `target`. Repeat until `target` is found or all adjacent nodes checked.
  */
+func pathExists<T: Comparable>(from: GraphNode<T>, to: GraphNode<T>) -> Bool {
+    var toVisit = [GraphNode<T>]()
+    toVisit.append(from)
+
+    repeat {
+        let aNode = toVisit.remove(at: 0)
+        guard aNode.value != to.value else { return true }
+        guard !aNode.visited else { continue }
+        aNode.visited = true
+
+        for node in aNode.adjacent {
+            guard !node.visited else { continue }
+            toVisit.append(node)
+        }
+    } while toVisit.count > 0
+
+    return false
+}
+
+/*:
+ To test `pathExists()`, we will use this simple graph
+
+ ![Simple Graph](Simple%20Graph.png)
+ */
+print("\n\n---------- 4.1 Route Between Nodes ----------")
+func createGraph() -> [GraphNode<Int>] {
+    let nodes = [
+        GraphNode<Int>(value: 0),
+        GraphNode<Int>(value: 1),
+        GraphNode<Int>(value: 2),
+        GraphNode<Int>(value: 3),
+        GraphNode<Int>(value: 4),
+        GraphNode<Int>(value: 5)
+    ]
+
+    nodes[0].adjacent = [nodes[1], nodes[4], nodes[5]]
+    nodes[1].adjacent = [nodes[3], nodes[4]]
+    nodes[2].adjacent = [nodes[1]]
+    nodes[3].adjacent = [nodes[2], nodes[4]]
+    nodes[4].adjacent = []
+    nodes[5].adjacent = []
+
+    return nodes
+}
+
+var graph = createGraph()
+print("pathExists(from: graph[0], to: graph[3]): \(pathExists(from: graph[0], to: graph[3]))")
+
+graph = createGraph()
+print("pathExists(from: graph[0], to: graph[2]): \(pathExists(from: graph[0], to: graph[2]))")
+
+graph = createGraph()
+print("pathExists(from: graph[1], to: graph[5]): \(pathExists(from: graph[1], to: graph[5]))")
+
+graph = createGraph()
+print("pathExists(from: graph[2], to: graph[4]): \(pathExists(from: graph[2], to: graph[4]))")
 
 //: ---
 //: [Previous](@previous)
