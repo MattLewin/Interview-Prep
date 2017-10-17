c91c6d7e524a91d2e597c0659e4f5b6d3b1b5ab8
Matt Lewin <matt@lewin.us>
2017-10-17 14:17:53 -0700

    Add "Data Structures" playground to the workspace. Implement a LRU cache using a doubly-linked list

---
+++ Data Structures.playground/Contents.swift
@@ -0,0 +1,124 @@
+import Foundation
+
+typealias DoublyLinkedListNode<T> = DoublyLinkedList<T>.Node<T>
+class DoublyLinkedList<T> {
+
+    class Node<T> {
+        var value: T
+        var previous: Node<T>?
+        var next: Node<T>?
+
+        init(value: T) {
+            self.value = value
+        }
+    }
+
+    private(set) var count = 0
+    private var head: Node<T>?
+    private var tail: Node<T>?
+
+    func addHead(_ value: T) -> Node<T> {
+        let node = Node(value: value)
+        defer {
+            head = node
+            count += 1
+        }
+
+        guard let head = self.head else {
+            tail = node
+            return node
+        }
+
+        head.previous = node
+        node.previous = nil
+        node.next = head
+
+        return node
+    }
+
+    func moveToHead(_ node: Node<T>) {
+        guard node !== head else { return }
+        let previous = node.previous
+        let next = node.next
+
+        next?.previous = previous
+        previous?.next = next
+        node.next = head
+        node.previous = nil
+        head?.previous = node
+
+        if node === tail {
+            tail = previous
+        }
+
+        head = node
+    }
+
+    func removeLast() -> Node<T>? {
+        guard let tail = self.tail else { return  nil }
+
+        let previous = tail.previous
+        previous?.next = nil
+        self.tail = previous
+
+        count -= 1
+
+        if count == 0 {
+            head = nil
+        }
+
+        return tail
+    }
+}
+
+class LRUCache<Key: Hashable, Value> {
+
+    private struct CachePayload {
+        let key: Key
+        let value: Value
+    }
+
+    private let list = DoublyLinkedList<CachePayload>()
+    private var nodesDict = [Key: DoublyLinkedListNode<CachePayload>]()
+
+    private let capacity: Int
+
+    init(capacity: Int) {
+        self.capacity = max(0, capacity)
+    }
+
+    func setValue(_ value: Value, for key: Key) {
+        let payload = CachePayload(key: key, value: value)
+
+        if let node = nodesDict[key] {
+            node.value = payload
+            list.moveToHead(node)
+        }
+        else {
+            let node = list.addHead(payload)
+            nodesDict[key] = node
+        }
+
+        if list.count > capacity {
+            let nodeRemoved = list.removeLast()
+            if let key = nodeRemoved?.value.key {
+                nodesDict[key] = nil
+            }
+        }
+    }
+
+    func getValue(for key: Key) -> Value? {
+        guard let node = nodesDict[key] else { return nil }
+
+        list.moveToHead(node)
+        return node.value.value
+    }
+}
+
+let cache = LRUCache<Int, String>(capacity: 2)
+cache.getValue(for: 1)
+cache.setValue("one", for: 1)
+cache.setValue("two", for: 2)
+cache.setValue("forty-two", for: 42)
+cache.getValue(for: 1)
+cache.getValue(for: 42)
